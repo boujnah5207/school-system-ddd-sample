0ea8cf94ba30a7bf3ead6c3e56869730548212f2
Mavixs@gmail.com <Mavixs@gmail.com@bc214954-3322-4bc0-debc-fa719e7439e6>
2014-02-18 18:46:50 +0000

    NuGet packages updates.

--- trunk/SchoolSystem.Web.UI/Global.asax.cs
+++ trunk/SchoolSystem.Web.UI/Global.asax.cs
@@ -5,13 +5,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace SchoolSystem.Web.UI
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication
     {
@@ -22,9 +19,7 @@ namespace SchoolSystem.Web.UI
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            Database.SetInitializer(new DropCreateDatabaseAlways<SchoolSystem.Infrastructure.EF.SchoolSystemDbContext>());
         }
     }
 }
